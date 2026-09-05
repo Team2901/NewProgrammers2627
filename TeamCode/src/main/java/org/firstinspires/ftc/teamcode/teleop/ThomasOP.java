7965d470458495ea04b19ebcfa6e78af7a9e93e8
leungt30370 <leungt30370@cghs.gmail>
2026-09-05 09:30:18 -0400

    right stick moves up down

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ThomasOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ThomasOP.java
@@ -2,18 +2,31 @@ package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(group = "Thomas")
 public class ThomasOP extends OpMode {
     int buttonPresses = 0;
+    DcMotor leftMotor;
+    DcMotor rightMotor;
 
     @Override
     public void init() {
+
         telemetry.addData("name", "ThomasLeung");
+
+        leftMotor = hardwareMap.get(DcMotor.class, "leftDrive");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightDrive");
+
+        leftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
+        leftMotor.setPower(gamepad1.right_stick_y);
+        rightMotor.setPower(gamepad1.right_stick_y);
 
         telemetry.addData("joyLx", gamepad1.left_stick_x);
 
