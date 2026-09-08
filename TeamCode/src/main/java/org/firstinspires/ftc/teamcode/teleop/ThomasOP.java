c61deffe94b1f8fe603cbfbb726fecbbcf47edfc
leungt30370 <leungt30370@cghs.gmail>
2026-09-08 16:28:04 -0400

    right stick moves up,down, left, and right

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ThomasOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ThomasOP.java
@@ -8,37 +8,35 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp(group = "Thomas")
 public class ThomasOP extends OpMode {
     int buttonPresses = 0;
-    DcMotor leftMotor;
-    DcMotor rightMotor;
+    DcMotor leftDrive;
+    DcMotor rightDrive;
 
     @Override
     public void init() {
 
         telemetry.addData("name", "ThomasLeung");
 
-        leftMotor = hardwareMap.get(DcMotor.class, "leftDrive");
-        rightMotor = hardwareMap.get(DcMotor.class, "rightDrive");
+        leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
 
-        leftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     @Override
     public void loop() {
-        leftMotor.setPower(gamepad1.right_stick_y);
-        rightMotor.setPower(gamepad1.right_stick_y);
+
+        double moveMultiplier = 0.45;
+        double forward = -gamepad1.right_stick_y;
+        double right= gamepad1.right_stick_x;
+
+        forward = forward * moveMultiplier;
+        right = right * moveMultiplier;
+
+        leftDrive.setPower(-gamepad1.right_stick_y + gamepad1.right_stick_x);
+        rightDrive.setPower(-gamepad1.right_stick_y - gamepad1.right_stick_x);
 
         telemetry.addData("joyLx", gamepad1.left_stick_x);
 
-        if (gamepad1.a) {
-            buttonPresses = buttonPresses + 1;
         }
-
-        if (gamepad1.a) {
-            telemetry.addData("message", "A is being pressed now");
-        } else {
-            telemetry.addData("message", "A is not being pressed now");
-        }
-        telemetry.addData("ButtonPresses", buttonPresses);
-    }
 }
